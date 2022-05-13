22aeb6d67b4bc7b191af04421e6964b96d126459
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-14 00:41:54 +0900

    Headerクラス作成

--- app/src/main/java/com/example/bjjrecord/view/atoms/Header.kt
+++ app/src/main/java/com/example/bjjrecord/view/atoms/Header.kt
@@ -2,11 +2,36 @@ package com.example.bjjrecord.view.atoms
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.example.bjjrecord.R
+import com.example.bjjrecord.databinding.ViewAtomsHeaderBinding
 
 class Header @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
-    private val
+    private var binding: ViewAtomsHeaderBinding? = null
+
+    var actionMyPage: OnClickListener? = null
+        set(value) {
+            field = value
+            binding?.actionMyPage = field
+        }
+
+    var actionTag: OnClickListener? = null
+        set(value) {
+            field = value
+            binding?.actionTag = field
+        }
+
+    init {
+        setBackgroundResource(R.color.white)
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.view_atoms_header,
+            this, true
+        )
+    }
 }
