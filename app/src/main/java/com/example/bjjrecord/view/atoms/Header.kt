236333f660335ad8c0f634e126780d615ebf01bd
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-14 00:25:57 +0900

    Headerクラス作成

---
+++ app/src/main/java/com/example/bjjrecord/view/atoms/Header.kt
@@ -0,0 +1,12 @@
+package com.example.bjjrecord.view.atoms
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Header @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+    private val
+}
